b7c3b104baa2069aebe00fa0953d2e44f971a3cf
Pamparau Cristian <cpamparau@stud.usv.ro>
2016-11-09 13:54:10 +0200

    [Change] My first commit here.

---
+++ MemoryGame/interfataCategorii/interfata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace interfataCategorii
+{
+    public interface interfataPentruCategorii
+    {
+        void deseneazaImagine(int numarTipImagine, int numarCopiiImagini);
+        void imagineClick(Object sender);
+        void ascundeImagini(PictureBox imagine);
+        void arataimagine(PictureBox imagine);
+        void nivelUsor();
+        void nivelMedium();
+        void nivelGreu();
+    }
+}
